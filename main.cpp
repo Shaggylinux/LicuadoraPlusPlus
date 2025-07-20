c531d18bc6e7c83ba9b03c70b4019f807d8a3254
shaggylinuxero <shaggylinuxero>
2025-07-20 10:16:23 -0500

    Agregacion de metodos y atributos a la licuadora

--- main.cpp
+++ main.cpp
@@ -1,4 +1,26 @@
 #include <iostream>
+#include <vector>
+#include <string>
+
+class VoidBlender{
+    private:
+        std::string name;
+        std::string model;
+        int modelnumber;
+    public:
+        VoidBlender() = default;
+        std::string GetName(){
+            return name;
+        }
+
+        std::string GetModel(){
+            return model;
+        }
+        
+        int GetModelNumber(){
+            return modelnumber;
+        }
+};
 
 int main(){
     std::cout << "hola mundo\n";
