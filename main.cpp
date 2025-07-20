e5e85fdbf643cf2fc0cf4052338de4d48edd7bc5
shaggylinux <malacruz132@gmail.com>
2025-07-20 11:05:36 -0500

    arreglando algunas cocitas XD

--- main.cpp
+++ main.cpp
@@ -9,7 +9,20 @@ class VoidBlender{
         int modelnumber;
     public:
         VoidBlender() = default;
-        std::string GetName(){
+        
+        void SetName(std::string _name){
+            name = _name;
+        }
+
+        void SetModel(std::string _model){
+            name = _model;
+        }
+
+        void SetModelNumber(std::string _ModelNumber){
+            name = _ModelNumber;
+        }
+
+         std::string GetName(){
             return name;
         }
 
@@ -20,8 +33,20 @@ class VoidBlender{
         int GetModelNumber(){
             return modelnumber;
         }
+
+        void Mostrarinfo(){
+            std::cout << name;
+            std::cout << model;
+            std::cout << modelnumber;
+        }
 };
 
 int main(){
-    std::cout << "hola mundo\n";
+    VoidBlender a, b, c, d, e, f, g;
+    std::vector<VoidBlender> licuadoras {a, b, c, d, e, f, g};
+    
+    for(int i{0} ; i < licuadoras.size() ; i++) {
+        licuadoras[i].SetModel("alv : ");
+    }
+    for(int i{0} ; i < licuadoras.size() ; i++) std::cout << licuadoras[i].GetName() << "\n";
 }
