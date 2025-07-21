a79c120a7efa3b0e69c7a33b5adc96729e0f87f6
shaggylinux <malacruz132@gmail.com>
2025-07-21 18:15:31 -0500

    Haciendo mejoras en el codigo

--- main.cpp
+++ main.cpp
@@ -35,18 +35,32 @@ class VoidBlender{
         }
 
         void Mostrarinfo(){
-            std::cout << name;
-            std::cout << model;
-            std::cout << modelnumber;
+            std::cout << "Name : " << name;
+            std::cout << "Model : " << model;
+            std::cout << "Model Number : " << modelnumber;
         }
 };
 
 int main(){
-    VoidBlender a, b, c, d, e, f, g;
-    std::vector<VoidBlender> licuadoras {a, b, c, d, e, f, g};
-    
-    for(int i{0} ; i < licuadoras.size() ; i++) {
-        licuadoras[i].SetModel("alv : ");
+    VoidBlender a, b;
+    std::vector<VoidBlender> licuadoras {a, b};
+    std::vector<std::string> namesblenders {"Oster", "Sams"};
+    int n;
+    std::cout << "##########\n";
+
+    for (int i{0} ; i < licuadoras.size() && namesblenders.size() ; i++){
+        n += 1;
+        std::cout << n << ". " << namesblenders[i] << "\n";
     }
-    for(auto licuadora : licuadoras) std::cout << licuadora.GetName() << "\n";
+    std::cout << "##########\n";
+
+    
+    /*
+        std::cout << "###########################################";
+        std::cout << " 1. Oster";
+        std::cout << " 2. NOSE";
+        std::cout << " 3. NOSE x2";
+        std::cout << " 4. NOSE x3";
+        std::cout << "###########################################";
+    */
 }
